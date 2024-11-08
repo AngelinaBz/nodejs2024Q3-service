056f0276c770046039c4f1149e9ca5356cbbd878
Angelina Bezik <angelina1337bz@gmail.com>
2024-11-08 18:07:02 +0300

    feat: implement users service and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdatePasswordDto } from './dtos/update-password.dto';
+import { isUUID } from 'class-validator';
 
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getHello(): string {
-    return this.usersService.getHello();
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException();
+    }
+    return this.usersService.findById(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    if (
+      !isUUID(id) ||
+      !updatePasswordDto.oldPassword ||
+      !updatePasswordDto.newPassword
+    ) {
+      throw new BadRequestException();
+    }
+    return this.usersService.update(id, updatePasswordDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    if (!isUUID(id)) {
+      throw new BadRequestException();
+    }
+    this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdatePasswordDto } from './dtos/update-password.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  private users: User[] = [];
+
+  findAll(): User[] {
+    return this.users.map((user) => ({ ...user, password: undefined }));
+  }
+
+  findById(id: string): User {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return { ...user, password: undefined };
+  }
+
+  create(createUserDto: CreateUserDto): User {
+    if (!createUserDto.login || !createUserDto.password) {
+      throw new BadRequestException();
+    }
+    const newUser: User = {
+      id: uuidv4(),
+      login: createUserDto.login,
+      password: createUserDto.password,
+      version: 1,
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    this.users.push(newUser);
+    return { ...newUser, password: undefined };
+  }
+
+  update(id: string, updatePasswordDto: UpdatePasswordDto): User {
+    const user = this.findById(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    if (user.password !== updatePasswordDto.oldPassword) {
+      throw new BadRequestException();
+    }
+    user.password = updatePasswordDto.newPassword;
+    user.updatedAt = Date.now();
+    user.version += 1;
+    return { ...user, password: undefined };
+  }
+
+  delete(id: string): void {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      throw new NotFoundException();
+    }
+    this.users.splice(userIndex, 1);
   }
 }
