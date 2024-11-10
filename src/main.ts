0b251b78059bc4ca6aee92ebf67d4c111c4c351c
Angelina Bezik <angelina1337bz@gmail.com>
2024-11-10 17:15:56 +0300

    feat: implement OpenAPI and change readme

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as yaml from 'yamljs';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const document: OpenAPIObject = yaml.load('./doc/api.yaml');
+  SwaggerModule.setup('doc', app, document);
   const port = process.env.PORT || 4000;
   await app.listen(port);
 }
