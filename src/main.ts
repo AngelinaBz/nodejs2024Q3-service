c304cc24d0ef57d9ca7f79711de529affdb2c795
Angelina Bezik <angelina1337bz@gmail.com>
2024-11-10 13:10:01 +0300

    refactor: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 4000;
   await app.listen(port);
 }
