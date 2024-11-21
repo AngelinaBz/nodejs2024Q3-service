92149c5c1948bb7993de1a8ad155b475f341914b
Angelina Bezik <angelina1337bz@gmail.com>
2024-11-21 19:42:29 +0300

    feat: update imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRE_TIME },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
